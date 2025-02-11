17488ddcddcd2d514ee725102b72f713b27473b6
Thai Phan <thaiphan0804@gmail.com>
2025-02-11 16:19:43 +0700

    init

---
+++ src/main/java/com/music/project/controllers/TestController.java
@@ -0,0 +1,32 @@
+package com.music.project.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+	
+
+	@GetMapping(value = "test")
+	public String test() {
+		return "ok";
+	}
+
+	
+
+}
---
+++ src/test/java/com/music/project/MusicProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
