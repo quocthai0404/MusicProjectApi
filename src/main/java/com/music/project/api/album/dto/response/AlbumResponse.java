33a68c0d781a46f1315b73ca778d8861cb21d8e9
baonghesi <baotrinh495@gmail.com>
2025-02-26 17:24:36 +0700

    bao_crud

--- src/main/java/com/music/project/api/album/dto/response/AlbumResponse.java
+++ src/main/java/com/music/project/api/album/dto/response/AlbumResponse.java
@@ -1,5 +1,22 @@
 package com.music.project.api.album.dto.response;
 
-public class AlbumResponse {
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AlbumResponse {
+    Integer id;
+    String name;
+    String slug;
+    String photo;
+    Date releaseDay;
+    Date createdAt;
+    Date updateAt;
 }
